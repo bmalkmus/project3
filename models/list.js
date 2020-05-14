5df14eaca9c0028f25d4f2f70550c5e615ad8ac2
Ta-Chung Chen <chenalbert35@gmail.com>
2020-05-13 19:14:57 -0700

    Create list.js to store lists.

---
+++ models/list.js
@@ -0,0 +1,51 @@
+var Sequelize = require("sequelize");
+var sequelize = require("../config/config.json");
+
+module.exports = function(sequelize, DataTypes) {
+    var List = sequelize.define("List", {
+      title: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          notEmpty: true
+        },
+      upc: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          notEmpty: true
+      },
+      description:{
+          type: DataTypes.TEXT,
+          allowNull: true
+      },
+      images:{
+          type:DataTypes.TEXT,
+          allowNull: false
+      },
+      platform:{
+          type:DataTypes.STRING,
+          allowNull: false
+      },
+      lnik:{
+          type:DataTypes.STRING,
+          allowNull: false
+      },
+      price: {
+          type:DataTypes.DECIMAL(10, 2),
+          allowNull: false
+      },
+      shipping:{
+        type:DataTypes.DECIMAL(4, 2),
+        allowNull: false
+      },
+      condition:{
+          type:DataTypes.CHAR,
+          allowNull: false
+      },
+      datefind:{
+          type: DataTypes.DATEONLY,
+          allowNull: false
+      }
+    });
+    return List;
+  };
+  
