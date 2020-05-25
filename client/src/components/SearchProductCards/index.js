970cdaa851ec7b50dd99b73530432b908054f769
Skyler Tosh <skylerjtosh@gmail.com>
2020-05-25 14:23:54 -0700

    Finished Bootstrap-React Search Cards

--- client/src/components/SearchProductCards/index.js
+++ client/src/components/SearchProductCards/index.js
@@ -8,6 +8,9 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 class SearchProductCards extends React.Component {
 	static contextType = DataAreaContext;
@@ -92,40 +95,56 @@ class SearchProductCards extends React.Component {
 		// Outer loop to create parent
 		for (let i = 0; i < this.context.products.length; i++) {
 			let card = (
-				<div className="card" key={i}>
-					<div className="card-header">
-						<div className="row">
-							<div className="col s6">
-								<h4 className="card-title">{this.context.products[i].item_attributes.title}</h4>
-							</div>
-							<div className="col s6">
-								<a href="#top" className="btn m-1 dropdown-toggle" data-toggle="dropdown">
-									<i>Save</i>
-								</a>
-								<ul className="dropdown-menu" id={this.context.products[i].item_attributes.title}>
-									<button id={this.context.products[i].item_attributes.title}>
-										{this.createDropDown(this.context.products[i])}
-									</button>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<div className="card-body">
-						<div className="row">
-							<div className="col s8">
-								<p className="category">{this.context.products[i].item_attributes.description}</p>
-							</div>
-							<div className="col s4">
+				<Container>
+					<Card className="card mx-auto p-3 mt-5" key={i}>
+						<Row className="no-gutters">
+							<Col className="md-4">
 								<img
 									src={this.context.products[i].item_attributes.image}
 									className="img-fluid"
 									alt="Responsive"
 								/>
-							</div>
-						</div>
-						{this.createPriceTags(this.context.products[i].Stores)}
-					</div>
-				</div>
+							</Col>
+							<Col>
+								<Card.Body>
+									<Card.Title>
+										<h4 className="card-title">{this.context.products[i].item_attributes.title}</h4>
+									</Card.Title>
+									<Card.Text>
+										<p className="category">
+											{this.context.products[i].item_attributes.description}
+										</p>
+									</Card.Text>
+								</Card.Body>
+							</Col>
+							<Card.Footer>
+								<Row>
+									<Dropdown>
+										<Dropdown.Toggle
+											href="#top"
+											className="btn m-1 dropdown-toggle"
+											variant="success"
+											id="dropdown-basic"
+										>
+											Save
+										</Dropdown.Toggle>
+
+										<Dropdown.Menu
+											className="dropdown-menu"
+											id={this.context.products[i].item_attributes.title}
+										>
+											<Dropdown.Item id={this.context.products[i].item_attributes.title}>
+												{this.createDropDown(this.context.products[i])}
+											</Dropdown.Item>
+										</Dropdown.Menu>
+									</Dropdown>
+								</Row>
+								<Row />
+								<Row>{this.createPriceTags(this.context.products[i].Stores)}</Row>
+							</Card.Footer>
+						</Row>
+					</Card>
+				</Container>
 			);
 			//Create the parent and add the children
 			cols.push(card);
@@ -140,7 +159,7 @@ class SearchProductCards extends React.Component {
 					<Card className="product-search-card">
 						<div className="input-field col s12">
 							<label className="m-0" htmlFor="search">
-								<h5 class="card-title">Product Search</h5>
+								<h5 className="card-title">Product Search</h5>
 							</label>
 							<Form className="search-form-control p-5">
 								<FormControl
