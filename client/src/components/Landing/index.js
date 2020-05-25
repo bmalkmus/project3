99d426e1c6960a17f043452b15c2998e015298e7
Skyler Tosh <skylerjtosh@gmail.com>
2020-05-25 13:39:26 -0700

    Moving welcome to a container

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import './style.css';
-import Search from "../Search"
-import Register from "../Register"
+import Search from '../Search';
+import Register from '../Register';
 
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
-
-function Landing () {
-
-    
-    return (
-        <div>
-            <div>
-                <div>
-                    <h1>WELCOME</h1>
-                    
-                </div>
-            </div>
-            {localStorage.usertoken ? <Search/> : <Register/>}
-        </div>
-    )
+function Landing() {
+	return (
+		<div>
+			<div>
+				<div>
+					<Container>
+						<Row>
+							<Card className="mx-auto p-3 mt-5" >
+								<Card.Title>
+									<h1>WELCOME</h1>
+								</Card.Title>
+							</Card>
+						</Row>
+					</Container>
+				</div>
+			</div>
+			{localStorage.usertoken ? <Search /> : <Register />}
+		</div>
+	);
 }
 
-export default Landing
+export default Landing;
