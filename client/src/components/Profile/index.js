e5eac8894e552036ba2a2f7e6315b1d4d7281f55
Skyler Tosh <skylerjtosh@gmail.com>
2020-05-25 19:11:32 -0700

    Formatted Profiles

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,85 +1,100 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import jwt_decode from 'jwt-decode';
-import API from "../utils/API";
-import SavedCard from '../SavedCard'
+import API from '../utils/API';
+import SavedCard from '../SavedCard';
 
-function Profile () {
-    const [first, setFirst] = useState();
-    const [last, setLast] = useState();
-    const [email, setEmail] =useState();
-    const [list, setList] =useState([]);
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
 
+function Profile() {
+	const [ first, setFirst ] = useState();
+	const [ last, setLast ] = useState();
+	const [ email, setEmail ] = useState();
+	const [ list, setList ] = useState([]);
 
-    const token = localStorage.usertoken;
-    const decoded = jwt_decode(token);
+	const token = localStorage.usertoken;
+	const decoded = jwt_decode(token);
 
-    function getInfo() {
-        setFirst(decoded.first_name);
-        setLast(decoded.last_name);
-        setEmail(decoded.email);
+	function getInfo() {
+		setFirst(decoded.first_name);
+		setLast(decoded.last_name);
+		setEmail(decoded.email);
 
-        API.UserList()
-        .then(res => {
-            setList(res.data)
-        })
+		API.UserList().then((res) => {
+			setList(res.data);
+		});
+	}
 
-    };
+	function deleteBTN(event) {
+		const id = event.target.dataset.id;
+		API.DeleteItem(id);
+		API.UserList().then((res) => {
+			setList(res.data);
+		});
+	}
 
-    function deleteBTN (event) {
-        const id = event.target.dataset.id
-        API.DeleteItem(id);
-        API.UserList()
-        .then(res => {
-            setList(res.data)
-        })
-    }
+	useEffect(
+		() => {
+			getInfo();
+		},
+		[ list ]
+	);
 
-    useEffect(() =>{
-        getInfo()
-    }, [list])
-
-    
-    return (
-        <div>
-            <div>
-                <h1>Profile</h1>
-            </div>
-            <table>
-                <tbody>
-                    <tr>
-                        <td>First Name</td>
-                        <td>{first}</td>
-                    </tr>
-                    <tr>
-                        <td>Last Name</td>
-                        <td>{last}</td>
-                    </tr>
-                    <tr>
-                        <td>Email Address</td>
-                        <td>{email}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <br/>
-            <div>
-                <h3>Saved List</h3>
-            </div>
-            {list.filter(product => product.user === decoded.email).map(product => (
-                <SavedCard 
-                    key = {product.id}
-                    id = {product.id}
-                    title = {product.title}
-                    description = {product.description}
-                    image = {product.images}
-                    platform = {product.platform}
-                    price = {product.price}
-                    shipping = {product.shipping}
-                    condition = {product.condition}
-                    deleteBTN = {deleteBTN}
-                />
-            ))}
-        </div>
-    )
+	return (
+		<div>
+			<Container>
+				<Row>
+					<Card className="mx-auto p-3 mt-5">
+						<Card.Title>
+							<h1>Profile</h1>
+						</Card.Title>
+						<Card.Body>
+							<table>
+								<tbody>
+									<tr>
+										<td>First Name</td>
+										<td>{first}</td>
+									</tr>
+									<tr>
+										<td>Last Name</td>
+										<td>{last}</td>
+									</tr>
+									<tr>
+										<td>Email Address</td>
+										<td>{email}</td>
+									</tr>
+								</tbody>
+							</table>
+						</Card.Body>
+					</Card>
+				</Row>
+			</Container>
+            <Container>
+                <Card className="mx-auto p-3 mt-5">
+                    <Card.Title>
+                        <h3>Saved List</h3>
+                    </Card.Title>
+                </Card>
+            </Container>
+			{list
+				.filter((product) => product.user === decoded.email)
+				.map((product) => (
+					<SavedCard
+						key={product.id}
+						id={product.id}
+						title={product.title}
+						description={product.description}
+						image={product.images}
+						platform={product.platform}
+						price={product.price}
+						shipping={product.shipping}
+						condition={product.condition}
+						deleteBTN={deleteBTN}
+					/>
+				))}
+		</div>
+	);
 }
 
-export default Profile
+export default Profile;
