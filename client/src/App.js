1673d956e6ff4002faac22b88b627ba3ba637921
bmalkmus <bmalkmus23@gmail.com>
2020-05-26 21:34:28 -0700

    fixed warning error

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from 'react';
 import jwt_decode from 'jwt-decode';
 import API from "./components/utils/API";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import NavbarComponent from './components/navbar';
 import Landing from './components/Landing';
 import Login from './components/Login';
 import Profile from './components/Profile';
 import Register from './components/Register';
 import Search from './components/Search';
+// import Button from 'react-bootstrap/Button';
+import Badge from "react-bootstrap/Badge";
 
 import './App.css';
 
@@ -17,6 +20,63 @@ function App() {
     const [ Saved, setSaved ] = useState([]);
 	let token;
 	let decoded = {email: "no email"}
+
+	// const history = useHistory();
+
+	
+	function NoToken() {
+		if (routes === true){
+			setRoutes(false);
+		}
+		return (
+			<div>
+				<li className="SignInLinks">
+					<Link className="link-tag" to="/register" id = "register1">
+						Register
+					</Link>
+				</li>
+				<li className="SignInLinks">
+					<Link className="link-tag" to="/login" id = "login1">
+						Login
+					</Link>
+				</li>
+			</div>
+		);
+	}
+	function HasToken() {
+		if (routes === false) {
+			setRoutes(true)
+		}
+
+		return (
+			<div>
+				<li className="SignInLinks">
+					<Link className="link-tag" to="/profile" id = "saved1">
+						Saved List
+						<Badge className = "notification" variant = "light">{notifications}</Badge>
+						<span className="sr-only">Saved Items</span>
+					</Link>
+				</li>
+				<li className="SignInLinks">
+					<Link className="link-tag" to="/search" id ="search1">
+						Search
+					</Link>
+				</li>
+
+				<li className="SignInLinks">
+					<Link onClick = {logOut} className = "link-tag" to= "/" id = "logout">Log Out</Link>
+					{/* <Button onClick={logOut} variant="light">Log out</Button> */}
+				</li>
+			</div>
+		);
+	}
+
+	function logOut(event) {
+		event.preventDefault();
+		console.log("logout click");
+		localStorage.removeItem('usertoken');
+		setRoutes(false)
+	}
 
     function getList() {
 		API.UserList()
@@ -46,7 +106,7 @@ function App() {
 		<Router>
 			<div className="main-container">
 				<header>
-					<NavbarComponent notifications = {notifications} setRoutes={setRoutes} routes = {routes} />
+					<NavbarComponent HasToken = {HasToken} NoToken = {NoToken} notifications = {notifications} routes = {routes}/>
 				</header>
 				<Switch>
 					<Route exact path={["/"]}>
@@ -56,7 +116,7 @@ function App() {
                           <Register/>
 					</Route>
 					<Route exact path={["/login"]}>
-                          <Login/>
+                          <Login setRoutes = {setRoutes} />
 					</Route>
 					<Route exact path={["/profile"]}>
 						{routes ? <Profile  Saved = {Saved} getList = {getList} /> : <Register />}
